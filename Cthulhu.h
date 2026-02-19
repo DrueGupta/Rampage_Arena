b3bfd57a0ccba5744d7284696bd991444c891346
Drue Gupta <1031762@my.scccd.edu>
2026-02-19 15:06:49 -0800

    Update Cthulhu.h

--- Cthulhu.h
+++ Cthulhu.h
@@ -17,9 +17,9 @@ public:
 		this->resource = 0;
 		this->resourceName = "Insanity";
 		this->specialOneName = "Mind breaker";
-		this->specialOneCost = 30;
-		this->specialTwoName = "Comprehend Infinity";
-		this->specialTwoCost = 60;
+		this->specialOneCost = 40;
+		this->specialTwoName = "Comprehend Mortality";
+		this->specialTwoCost = 90;
 		this->durabilityLossPercent = 0.10;
 	}
 	void startTurn() override
@@ -29,5 +29,58 @@ public:
 		{
 			this->resource = 200;
 		}
+	}
+	bool specialOne(Player* target) override
+	{
+		if (this->resource < this->specialOneCost)
+		{
+			cout << "Not enough " << this->resourceName << " to do that." << endl;
+			return false;
+		}
+		this->resource -= this->specialOneCost;
+		bool stunned = false;
+		for (Effect effect : target->getEffects())
+		{
+			if(effect.effectType == "STUN")
+			{
+				stunned = true;
+			}
+			if (stunned)
+			{
+				cout << target->getName() << " is stunned and cannot hurt themselves" << endl;
+				return false;
+			}
+		}
+		int ogCritChance = target->getCritChance();
+		int ogDodge = target->getDodge();
+		int ogToHit = target->getToHit();
+		bool dead = false;
+		cout << this->name << " used their psychic powers to implant the knowledge of the Old in " << target->getName() 
+			<< ", driving them insane and causing them to hurt themselves." << endl;
+		target->setCritChance(100);
+		target->setToHit(100);
+		target->setDodge(0);
+		target->attack(target, "PHYS");
+		target->setCritChance(ogCritChance);
+		target->setDodge(ogDodge);
+		target->setToHit(ogToHit);
+		return dead;
+	}
+	bool specialTwo(Player* target) override
+	{
+		if (this->resource < this->specialTwoCost)
+		{
+			cout << "Not enough " << this->resourceName << " to do that." << endl;
+			return false;
+		}
+		this->resource -= this->specialTwoCost;
+		Effect death;
+		death.effectType = "MORT";
+		death.effectDMG = 0;
+		death.duration = rand() % 5 + 4;
+		cout << this->name << " cast an ancient spell on " << target->getName() << ". " << target->getName()
+			<< " lifespan has been shorten by an exponential amount. How much is unknown, but what they do know is that they don't have long..." << endl;
+		target->getEffects().push_back(death);
+		return false;
 	}
 };
